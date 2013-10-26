8ef188e33b97d251257227c54a955f41983f5c79
longluo <calmbody@163.com>
2013-10-26 19:08:09 +0800

    Adding the Android and iOS pages.

---
+++ iOS/ios.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<!-- Copyright &copy; 2005-2013 Long Luo. All rights reserved. -->
+<html>
+	<head>
+		<title> iOS Projects </title>		
+	</head>
+	<body>
+		<h1>
+			Still Working...
+		</h1>
+		<p>
+		</p>
+		<?php
+			//echo "Hello PHP!";
+		?>
+		<h6 style="color:blue; text-align:center">Copyright &copy; 2005-2013 Long Luo. All rights reserved.</h6>
+	</body>
+</html>
